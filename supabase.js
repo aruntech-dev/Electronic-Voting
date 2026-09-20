a087c794826171ea585894731aac1687b0a743cd
aruntech-dev <pinkiyadav98207309@gmail.com>
2026-09-20 11:03:09 +0545

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-const SUPABASE_URL = "https://vnkwylrwnotybxzguolb.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_nrRE6i2QcollvLI5bNKluA_jXE2UspR";
-const { createClient } = supabase;
-const db = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
-async function getSession(){const {data,error}=await db.auth.getSession();if(error)throw error;return data.session;}
-async function signOut(){await db.auth.signOut();location.href="index.html";}
